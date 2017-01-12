31367bc4d33485f061b81a4c40bc796d847ad4b1
gcnew <gcnew@abv.bg>
2017-01-12 02:46:41 +0200

    Improved unification in case of excluded arguments

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -15077,7 +15077,12 @@ namespace ts {
                         argType = checkExpressionWithContextualType(arg, paramType, mapper);
                     }
 
+                    // turn off type logging if the argument is excluded
+                    const cachedLog = (<InfExt> context.inferences).log;
+                    excludeArgument && excludeArgument[i] !== undefined && ((<InfExt> context.inferences).log = undefined);
+
                     inferTypesWithContext(context, argType, paramType);
+                    excludeArgument && excludeArgument[i] !== undefined && ((<InfExt> context.inferences).log = cachedLog);
                 }
             }
 
